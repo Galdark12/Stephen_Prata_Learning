3a236b92c145a4235e235070c7848d76f947e11f
Galdark12 <Galdarkk@gmail.com>
2017-01-02 20:28:35 +0200

    Learning Prata Exerceise 4_7

---
+++ Prata_4_7.cpp
@@ -0,0 +1,43 @@
+// Prata_4_7.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+
+struct pizza_example
+{
+	string company_name;
+	double diameter;
+	double pizza_weight;
+};
+
+
+int main()
+{
+	pizza_example test_struct;
+
+	cout << "Please enter company name: " << endl;
+	getline(cin, test_struct.company_name);
+
+	cout << "Please enter pizza diameter: " << endl;
+	cin >> test_struct.diameter;
+
+	cout << "Please enter pizza weight: " << endl;
+	cin >> test_struct.pizza_weight;
+
+	cout << "*******************************" << endl;
+	cout << "Name: " << test_struct.company_name << endl;
+	cout << "Diameter: " << test_struct.diameter << endl;
+	cout << "Weight: " << test_struct.pizza_weight << endl;
+	cout << "*******************************" << endl;
+
+	system("pause");
+	
+
+    return 0;
+}
+
