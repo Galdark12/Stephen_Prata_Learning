216dbbade6694bb7ee5fb54e2fc271daeb7b58d4
Galdark12 <Galdarkk@gmail.com>
2017-01-04 18:12:42 +0200

    Prata5_7
    Structure that describes the collection and car number insertion;

---
+++ Prata5_7.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+struct car
+{
+	std::string producerName;
+	int productionDate;
+};
+
+int main()
+{
+	std::cout << " Enter the number of cars in your colection! " << std::endl;
+	int carsQuantity;
+	std::cin >> carsQuantity;
+
+}
