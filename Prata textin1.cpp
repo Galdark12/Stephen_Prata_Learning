8ccd0cc33e18ab4d843cdda363429817628e5c55
Galdark12 <Galdarkk@gmail.com>
2017-01-04 16:57:23 +0200

    Textin with cin.get() function

--- Prata textin1.cpp
+++ Prata textin1.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
 #include <cstdlib>
 
+// Modified with cin.get() function placed in istream class;
+// cin.get() detect whitespaces and count them as 1 symbol;
 int main()
 {
 	char charWeGet;
 	int iterationCounter = 0;
 	std::cout << " Enter the characters. To fonish press '=' " << std::endl;
-	std::cin >> charWeGet;
+	std::cin.get(charWeGet);
 
 	while (charWeGet != '=')
 	{
 		std::cout << charWeGet;
 		iterationCounter++;
-		std::cin >> charWeGet;
+		std::cin.get(charWeGet);
 	}
 
-	std::cout << "Congrat, program finished. The number of chars entered is " << iterationCounter << std::endl;
+	std::cout << std::endl;
+	std::cout << " Congrat, program finished. The number of chars entered is " << iterationCounter << std::endl;
 
 	system("pause");
 	return 0;
